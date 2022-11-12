0a1364e411ac0f568cb6a85a4e837a6e350a0502
Rodrigo Sandeski <sandeski47@gmail.com>
2022-11-12 16:13:45 -0300

    first commit

---
+++ empresa/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Tecnologias, Empresa, Vagas
+
+
+admin.site.register(Tecnologias)
+admin.site.register(Empresa)
+admin.site.register(Vagas)
+# Register your models here.
